77e7cddd33e04462b6fd7f2aaa18aa77676c09e3
shehbazkhanani <cuteboyusman49@gmail.com>
2022-09-02 21:37:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,329 @@
+// Write a script to greet your website visitor using JS alert box.
+// alert("Error! Please enter a valid password");
+
+// Write a script to display following message on your web page: (Hint : Use line break)
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// Write a script to display following messages in sequence:
+// alert("Welcome to Js land...");
+// var msg = {Msg:"Happy Coding!"};
+// alert(msg.Msg, oCheckbox.msg)
+// console.log({msg, oCheckbox:msg});
+
+// Generate the following message through browser’s developer console:
+// console.log(alert("Hello... I can run JS through my web browser's console"));
+
+// Declare a variable called username
+// var username = "shehbazkhanani";
+// console.log(username);
+
+// Declare a variable called myName and assign to it a string that represents your Full Name.
+// var myName = "Shehbaz";
+//     fullname = myName + " khanani";
+//     console.log(fullname);
+
+// write script and declare a variable and assign into alert
+// var message = "Hello World";
+// alert(message);
+
+// Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// var studentName = "shehbaz khanani";
+// var studentAge = "24 Year old";
+// var studentBio = "will be certified Mobile Application Development";
+
+// alert(studentName);
+// alert(studentAge);
+// alert(studentBio);
+
+// Write a script to display the following alert using one JS variable
+// var a = "pizza";
+// var b = a.slice(0,5).toUpperCase();
+//     console.log(b)
+// var c = b.slice(0,4);
+//     console.log(c);
+// var d = b.slice(0,3);
+//     console.log(d);
+// var e = b.slice(0,2);
+// var f = b.slice(0,1);
+// alert(b +"\n" + c + "\n" + d + "\n" + e + "\n" + f)
+
+// var email = "example@example.com";
+// alert("My email address is " + email);
+
+// var book = "A smarter way to learn JavaScript";
+// alert(book);
+
+// var script = "Yah! I can write HTML content through JavaScript";
+// document.write(script);
+
+// var JS = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(JS);
+
+// var age = 15;
+// alert("i am " + age + " year old");
+
+// var visitor = 14;
+// alert("You have visited this site " + visitor + " times");
+
+// var year = +prompt("please input your birth year here");
+// document.write("My birth year is " + year );
+
+// var visitor = prompt("Visitor's name");
+// var title = prompt(" Product Title");
+// var quantity = +prompt("Order's Quantity");
+// var store = "XYZ clothing.";
+// document.write(visitor + " ordered " + quantity  + " " + title + " on " + store);
+
+// var firstName = "shehbaz";
+// var surname = "khanani";
+// var age = 24;
+
+// var username = firstName + surname + age;
+// document.write(username);
+
+// legal 
+// var ab4 = "";
+// var a$bc = "";
+// var $abc = "";
+// var _abc = "";
+// var _$ = "";
+
+// illegal 
+// var a^bc = "";
+// var a%bc = "";
+// var 1bc = "";
+// var .ab = "";
+// var ab-c = "";
+
+// Rules for naming JS variable
+// var heading = "Rules for naming JS variable";
+// var a = "$";
+// var b = "_";
+// var c = "$my";
+// var d = "_1st";
+// document.write("<h3>"+heading+"</h3>")
+// document.write("Variable names can only cintan. numbers, " + a + " " + b + " " + c + " For example: " + d + " Variable <br>");
+// document.write("Variable must begin with a letter. " + a + " or " + b + " For example : " +b+"name, " + a + "name or name <br>");
+// document.write("Variable names are case sensitive <br> Variable names should not be JS Keywords")
+
+// var a = 3;
+// var b = 5;
+// var c = a + b;
+// document.write("sum of " + a + " and " + b + " is " + c );
+
+// var a = 3;
+// var b = 5;
+// var c = a - b;
+// document.write("sub of " + a + " and " + b + " is " + c );
+
+// var a = 3;
+// var b = 5;
+// var c = a * b;
+// document.write("mul of " + a + " and " + b + " is " + c );
+
+// var a = 3;
+// var b = 5;
+// var c = a / b;
+// document.write("div of " + a + " and " + b + " is " + c );
+
+// var value = 1;
+// document.write("Value after variable declaration is undefined <br>")
+// var add = 5;
+// document.write("Initialize the value is: " + add + "<br>")
+// var increment = value + add;
+// document.write("value after increment is: " + increment + "<br>")
+// var addMore = 7;
+// var increment1 = increment + addMore;
+// document.write("after addition is: " + increment1 + "<br>")
+// var val = 1;
+// var decrement = increment1 - val;
+// document.write("after decrement is " + decrement + "<br>");
+// var val1 = 3;
+// var div = (decrement%val1);
+// document.write("The remainder is " + div);
+
+// var price = 600;
+// var Quant = 5;
+// var ticket = price*Quant;
+// document.write("Total cost to buy " +Quant+ " tickets to a movie is " + ticket +"PKR" );
+
+
+// document.write("<h3> Table of 4 </h3>")
+// var val = 1;
+// for (let i=0; i<10; i++) {
+//         a = val++;
+//         b = 4;
+//         c = a*b;
+//     document.write("4 X "+a+" = " + c +"<br>")
+// }
+
+// var table = Number(prompt("input value"))
+// var val = 1;
+// for (let i=0; i<10; i++) {
+//         a = val++;
+//         b = table;
+//         c = a*b;
+//     document.write(table+ "X" +a+" = " + c +"<br>")
+// }
+
+
+// var C = 25;
+// var F = 70;
+// var oC = (F-32)*5/9;
+// var oF = (C*9/5)+32;
+// document.write(C + "\xB0C is " +oF+"\xB0F<br>");
+// document.write(F + "\xB0F"+ " is "+ oC+"\xB0C");
+
+// var item1 = 650;
+// var item2 = 100;
+// var order1 = 3;
+// var order2 = 7;  
+// var charges = 100;
+
+// document.write("Price of item 1 is "+ item1 +"<br>")
+// document.write("Quantity of item 1 is "+ order1 +"<br>")
+// document.write("Price of item 2 is "+ item2 +"<br>")
+// document.write("Quantity of item 1 is "+ order2 +"<br>")
+// document.write("Shipping Charges "+ charges +"<br> <br>")
+
+// var Total = (item1*order1)+(item2*order2)+charges;
+// document.write("Total cost of your order "+ Total);
+
+// var marks = 980;
+// var obtained = 804;
+// var percentage = (obtained/marks)*100;
+
+// document.write("Total marks: " + marks +"<br> Marks obtianed: " +obtained+"<br> Percentage: " + percentage);
+
+// var US = 10;
+// var Riyal = 25;
+// var USPKR = 104.80;
+// var RPKR = 28;
+// var PKR = (USPKR*US) + (RPKR*Riyal)
+// document.write(`Total Currency in PKR: ${PKR}`);
+
+// var a = 1;
+// var b = 5+a;
+// var c = b*10;
+// var d = c/30;
+// console.log(d);
+
+// const d = new Date();
+// var year = d.getFullYear();
+// var birth = 1997;
+// var dist = year - birth;
+// document.write(`<h1> The Age Calculater </h1> <br> Current Year: ${year} <br> Birth Year: ${birth} <br> Your age is: ${dist}`)
+
+// let pi = 3.142;
+// var radius = 20;
+// var circumference = (2*pi*radius);
+// var area = (pi*(radius*radius));
+// document.write(`<h1> The Grometerizer </h1> <br> Radious of a circle is: ${radius} <br> The circumference is: ${circumference} <br> The area is: ${area}`)
+
+// var favouriteSnaks = "Lays";
+// var currentAge = 25;
+// var estimatedAge = 50;
+// var snaksPerDay = 2;
+
+// document.write(`<h1> The Lifetime Supply Calculator: </h1>
+// <p> Favourite Snack: ${favouriteSnaks} </p> 
+// <p> Current age: ${currentAge} </p> 
+// <p> Estimated Maximum Age: ${estimatedAge} </p> 
+// <p> Amount of snaks per day: ${snaksPerDay} </p> 
+// <p> You will need  ${(estimatedAge-currentAge)*snaksPerDay} lays to last you until the ripe old age of ${estimatedAge}  </p> `
+// )
+
+// let a = 10;
+// console.log(a)
+// let b = a;
+// let c = ++b;
+// let d = --a;
+// let e = a--;
+// document.write(`
+// initial value of a is: ${a} <br>
+// Now the value of a is: ${c} <br>
+// Now the value of a is: ${d} <br>
+// Now the value of a is: ${e}`) 
+
+// var username = prompt("inpit your username");
+// alert(`Hello ${username} !`);
+
+// var table = Number(prompt("input value"))
+// var val = 1;
+
+// if (table == "") {
+// for (let i=0; i<10; i++) {
+//         a = val++;
+//         e = 5;
+//         d = a*e;
+//         document.write(e+ "X" +a+" = " + d +"<br>")
+    
+// }} else {
+//     for (let i=0; i<10; i++) {
+//         a = val++;
+//         b = table;
+//         c = a*b;
+//         document.write(table+ "X" +a+" = " + c +"<br>")
+//     }
+// }
+
+// var table = Number(prompt("input value") || 5)
+// var val = 1;
+// for (let i=0; i<10; i++) {
+//             a = val++;
+//             e = table;
+//             d = a*e;
+//             document.write(e+ "X" +a+" = " + d +"<br>") 
+//         }
+
+// let english = 100;
+// let math = 100;
+// let urdu = 100;
+// let engObt = Number(prompt(""))
+// let mathObt = Number(prompt(""));
+// let urduObt = Number(prompt(""));
+
+// document.write(`<h1> Subject Total Marks Obtained Marks Percentage </h1> 
+// <span> Englsih </span> <span> ${english} </span> <span> ${engObt} </span> <span> ${(english*engObt)/100}% </span> <br>
+// <span> Math </span> <span> ${math} </span> <span> ${mathObt} </span> <span> ${(math*mathObt)/100}% </span> <br>
+// <span> Urdu </span> <span> ${urdu} </span> <span> ${urduObt} </span> <span> ${(urdu*urduObt)/100}% </span> <br>
+// <span> Total </span> <span> ${english+math+urdu} </span> <span> ${engObt+mathObt+urduObt} </span> <span> ${engObt+mathObt+urduObt}% </span>`
+// )
+
+// var karachi = prompt("Please enter city Name");
+// var value = karachi.toUpperCase();
+// if (value == "KARACHI") {
+//     alert("welcome to city of light");
+// } else {
+//     alert(`welcome to ${karachi}`);
+// }
+
+// var value = prompt("please enter your gender");
+// var gender = value.toUpperCase();
+// if (gender == "MALE") {
+//     alert(`Good Morning Sir`)
+// } else if (gender == "FEMALE") {
+//     alert(`Good Morning Maam`)
+// } else {
+//    alert(`Good Morning ${gender}`)
+// }
+
+// var value = prompt("Please ender your condition");
+// var signal = value.toUpperCase();
+// if (signal == "RED") {
+//     alert(`Must Stop`)
+// } else if ( signal == "YELLOW") {
+//     alert(`Ready To Move`)
+// } else if ( signal == "GREEN") {
+//     alert(`Move Now`)
+// } else {
+//     alert(`Enter Right Condition`)
+// }
+
+// var value = prompt("Enter Fuel Remain"); 
+// if (value == 0.25) {
+//     alert(`Please refill the fuel in your car`)
+// } else {
+//     alert(`you have enough fuel to drive`)
+// }
+
